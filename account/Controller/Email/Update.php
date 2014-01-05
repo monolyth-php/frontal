97aa4bb0e82057a33538338eddc11998a507ce09
Marijn Ophorst <marijn@sensimedia.nl>
2014-01-05 17:03:20 +0100

    modernize

--- account/Controller/Email/Update.php
+++ account/Controller/Email/Update.php
@@ -8,9 +8,16 @@
 namespace monolyth\account;
 use monolyth\Controller;
 use monolyth\Login_Required;
+use monolyth\Message;
 
 class Update_Email_Controller extends Controller implements Login_Required
 {
+    public function __construct()
+    {
+        parent::__construct();
+        $this->form = new Update_Email_Form;
+    }
+
     protected function get(array $args)
     {
         return $this->view('page/email/update');
@@ -19,14 +26,15 @@ class Update_Email_Controller extends Controller implements Login_Required
     protected function post(array $args)
     {
         if (!$this->form->errors()) {
-            if (!($error = $this->account->email($this->form))) {
-                $this->message->add(
-                    self::MESSAGE_SUCCESS,
+            $account = new User_Model;
+            if (!($error = $account->email($this->form))) {
+                self::message()->add(
+                    Message::SUCCESS,
                     $this->text('./confirm')
                 );
             } else {
-                $this->message->add(
-                    self::MESSAGE_ERROR,
+                self::message()->add(
+                    Message::ERROR,
                     $this->text("./error.$error")
                 );
             }
