92d18fc754908db53f3eee9f1f8d367a0470440b
Marijn Ophorst <marijn@sensimedia.nl>
2014-01-05 11:09:35 +0100

    modernize

--- utils/Translatable.php
+++ utils/Translatable.php
@@ -1,18 +1,28 @@
 <?php
 
 namespace monolyth\utils;
+use monolyth\Text_Model;
+use monolyth\Language_Access;
 
 trait Translatable
 {
     public function text($id)
     {
-        return call_user_func_array($this->text, func_get_args());
+        static $text;
+        if (!isset($text)) {
+            $text = new Text_Model($this);
+        }
+        return call_user_func_array($text, func_get_args());
     }
 
     public function textLanguage($id, $language)
     {
+        static $text;
+        if (!isset($text)) {
+            $text = new Text_Model($this);
+        }
         return call_user_func_array(
-            [$this->text, 'getByLanguage'],
+            [$text, 'getByLanguage'],
             func_get_args()
         );
     }
