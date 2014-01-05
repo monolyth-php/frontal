4a43d8c695a27c2372d648b279f9a4a533be87ec
Marijn Ophorst <marijn@sensimedia.nl>
2014-01-05 12:01:26 +0100

    prevent __clone and __wakeup for singletons

--- core/Singleton.php
+++ core/Singleton.php
@@ -13,5 +13,21 @@ trait Singleton
         }
         return $instance;
     }
+
+    public function __clone()
+    {
+        trigger_error(
+            'Cloning '.__CLASS__.' is not allowed.',
+            E_USER_ERROR
+        );
+    }
+    
+    public function __wakeup()
+    {
+        trigger_error(
+            'Unserializing '.__CLASS__.' is not allowed.',
+            E_USER_ERROR
+        );
+    }
 }
 
