120f9fe4d8a38e7af60aa32adb76030d41f16d16
Marijn Ophorst <marijn@sensimedia.nl>
2014-01-05 11:02:31 +0100

    modernize

--- Access/Country.php
+++ Access/Country.php
@@ -2,7 +2,15 @@
 
 namespace monolyth;
 
-interface Country_Access
+trait Country_Access
 {
+    public function country()
+    {
+        static $country;
+        if (!isset($country)) {
+            $country = new Country_Model;
+        }
+        return $country;
+    }
 }
 
