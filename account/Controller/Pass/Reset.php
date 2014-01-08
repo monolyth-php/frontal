6618d132e0b3dad6b607c5d1e9dd234e2af83785
Marijn Ophorst <marijn@sensimedia.nl>
2014-01-08 18:47:20 +0100

    modernize

--- account/Controller/Pass/Reset.php
+++ account/Controller/Pass/Reset.php
@@ -11,34 +11,44 @@ use monolyth\Controller;
 use monolyth\adapter;
 use monolyth\render\Url_Helper;
 use monolyth\Nologin_Required;
+use monolyth\Confirm_Model;
+use monolyth\Message;
 
 class Reset_Pass_Controller extends Controller
 implements adapter\Access, Nologin_Required
 {
+    public function __construct()
+    {
+        parent::__construct();
+        $this->form = new Reset_Pass_Form;
+        $this->confirm = new Confirm_Model;
+        $this->pass = new Pass_Model;
+    }
+
     protected function get(array $args)
     {
         extract($args);
         $this->form->addSource(compact('id', 'hash'))->load();
         if (!($error = $this->confirm->process($hash, $id))) {
-            $pw = $this->adapter->field(
+            $pw = self::adapter()->field(
                 'monolyth_auth',
                 'pass',
                 compact('id')
             );
             $this->pass->update($pw, $id);
-            $this->message->add(
-                self::MESSAGE_SUCCESS,
+            self::message()->add(
+                Message::SUCCESS,
                 $this->text('pass/reset/success', ['pass' => $pw])
             );
             return $this->view('page/pass/display', ['pass' => $pw]);
         } elseif ($error == 'contains outdated elements') {
-            $this->message->add(
-                self::MESSAGE_ERROR,
+            self::message()->add(
+                Message::ERROR,
                 $this->text('pass/reset/error.date')
             );
         } else {
-            $this->message->add(
-                self::MESSAGE_ERROR,
+            self::message()->add(
+                Message::ERROR,
                 $this->text('pass/reset/error.generic')
             );
         }
