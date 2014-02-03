dba4b5a3d97b4c8906eb4a93165bb4fcad5a76dc
Marijn Ophorst <marijn@sensimedia.nl>
2014-02-03 14:44:36 +0100

    oops, this has become a singleton

--- account/Model/Pass/Reset.php
+++ account/Model/Pass/Reset.php
@@ -14,7 +14,7 @@ use monolyth\render\Url_Helper;
 use monolyth\Config;
 use monolyth\Confirm_Model;
 use monolyth\Language_Access;
-use monolyth\render\Email_Access;
+use monolyth\render\Email;
 use Project;
 
 /**
@@ -26,7 +26,6 @@ class Reset_Pass_Model extends Model
     use Url_Helper;
     use User_Access;
     use Language_Access;
-    use Email_Access;
 
     public function __construct()
     {
@@ -106,7 +105,7 @@ class Reset_Pass_Model extends Model
                     ]
                 );
             }
-            $email = self::email();
+            $email = Email::instance();
             $email->setSource($mail)
                   ->setVariables([
                       'name' => $auth['name'],
