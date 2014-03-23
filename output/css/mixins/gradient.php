926fea49f3fe6cb27a1cb9e519efa2fcd1341216
Marijn Ophorst <marijn@sensimedia.nl>
2014-03-23 18:39:39 +0100

    Squashed commit of the following:
    commit bd4ba32ce0b387ae9890fc6490742976cd82bc3e
    Author: Marijn Ophorst <marijn@sensimedia.nl>
    Date:   Sun Mar 23 18:39:24 2014 +0100
    
        make the gradient mixin more versatile

--- output/css/mixins/gradient.php
+++ output/css/mixins/gradient.php
@@ -1,21 +1,73 @@
 <?php
 
 /**
- * A simple mixin for CSS3 background gradients.
- * This only supports linear gradients in two colours. If you need anything
- * more complex, you'll need to roll your own :)
+ * A mixin for CSS3 background gradients.
+ * There is only one required argument: $fallback, being your desired
+ * fallback colour if gradients are unsupported in the current browser.
+ * The optional second argument can be either 'vertical' (top-down) or
+ * 'horizontal' (left-right), defaulting to 'vertical'.
+ * All subsequent arguments are of the form 'color:stop'.
+ * Older internet explorers get a guesstimated simple gradient between
+ * the lightest and darkest colour supplied (based on whichever comes first).
  */
-return function($from, $to) {
-    return <<<EOT
-background: $to;
-background: -moz-linear-gradient(top, $from, $to 100%);
-background: -webkit-gradient(linear, left top, left bottom, color-stop(0%, $from), color-stop(100%, $to));
-background: -webkit-linear-gradient(top, $from 0%, $to 100%);
-background: -o-linear-gradient(top, $from 0%, $to 100%);
-background: -ms-linear-gradient(top, $from 0%, $to 100%);
-background: linear-gradient(to bottom, $from 0%, $to 100%);
--ms-filter: "progid:DXImageTransform.Microsoft.gradient(startColorStr='$from', endColorStr='$to', GradientType=0)";
-filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='$from', endColorstr='$to',GradientType=0);
+return function($fallback, $type = 'vertical') {
+    $args = func_get_args();
+    $fallback = array_shift($args);
+    $type = trim(array_shift($args));
+    $default_vendor_prefixed = function($prefix, $type, $stops) {
+        $out = "background: -$prefix-linear-gradient(";
+        if ($type == 'vertical') {
+            $out .= 'top';
+        } else {
+            $out .= 'left';
+        }
+        foreach ($stops as $arg) {
+            list($colour, $stop) = explode(':', $arg);
+            $out .= ", $colour $stop";
+        }
+        $out .= ");\n";
+        return $out;
+    };
+
+    $out = '';
+    foreach (['moz', 'o', 'ms'] as $prefix) {
+        $out .= $default_vendor_prefixed($prefix, $type, $args);
+    }   
+    $out .= "background: -webkit-gradient(linear, ";
+    if ($type == 'vertical') {
+        $out .= "left top, left bottom";
+    } else {
+        $out .= "left top, right top";
+    }
+    foreach ($args as $arg) {
+        list($colour, $stop) = explode(':', $arg);
+        $out .= ", color-stop($stop, $colour)";
+    }
+    $out .= ");\n";
+    $out .= "background: linear-gradient(";
+    if ($type == 'vertical') {
+        $out .= "to bottom";
+    } else {
+        $out .= "90deg";
+    }
+    $out .= ", ";
+    foreach ($args as $arg) {
+        list($colour, $stop) = explode(':', $arg);
+        $out .= ", $colour $stop";
+    }
+    $out .= <<<EOT
+);
+background-color: $fallback;
+
 EOT;
+    $start = explode(':', array_shift($args))[0];
+    $end = explode(':', array_pop($args))[0];
+    $type = $type == 'vertical' ? 0 : 1;
+    $out .= <<<EOT
+-ms-filter: "progid:DXImageTransform.Microsoft.gradient(startColorStr='$start', endColorStr='$end', GradientType=0)";
+filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='$start', endColorstr='$end',GradientType=0);
+
+EOT;
+    return $out;
 };
 
