2a5c38310cd11b174e10770eae74f6c16dd51bba
Marijn Ophorst <marijn@sensimedia.nl>
2014-07-07 12:29:23 +0200

    route to proxy views for ng-include so we avoid symlinking a ton of crap into the public directory

--- api/config/routing.php
+++ api/config/routing.php
@@ -3,6 +3,7 @@
 namespace monolyth\api;
 
 return function($m) use($project) {
+    $m->connect('/html/(%s:language)/(%a:viewname).html', 'monolyth\api\View');
     $m->connect('/monolyth/form/(%s:language)/(%a:name)/', 'monolyth\api\Form');
     return $m;
 };
