f8b4d9442eb1bddb3cdc553997f86f5d4df6e2b7
Marijn Ophorst <marijn@sensimedia.nl>
2014-07-08 10:13:37 +0200

    make sure this exists

--- account/output/html/page/login.php
+++ account/output/html/page/login.php
@@ -6,6 +6,9 @@
  */
 
 namespace monolyth\account;
+if (!(isset($form) && $form instanceof Login_Form)) {
+    $form = new Login_Form;
+}
 echo $view(
     ['monolyth\render\form\slice/table', 'monolyth\render\form\slice/form'],
     compact('form')
