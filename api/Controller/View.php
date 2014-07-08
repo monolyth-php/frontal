5066860da2d844edf7b866401f8f7660d3ef6daa
Marijn Ophorst <marijn@sensimedia.nl>
2014-07-08 10:13:45 +0200

    handle namespaces

--- api/Controller/View.php
+++ api/Controller/View.php
@@ -11,7 +11,22 @@ class View_Controller extends Controller
     protected function get(array $args)
     {
         extract($args);
-        return $this->view("\\$viewname", ['parse' => true]);
+        $parts = preg_split(
+            "@/(page|slice|template)@",
+            $viewname,
+            3,
+            PREG_SPLIT_DELIM_CAPTURE
+        );
+        if (count($parts) == 3) {
+            $viewname = str_replace(
+                '/',
+                '\\',
+                $parts[0]
+            ).'\\'.$parts[1].$parts[2];
+        } else {
+            $viewname = "\\$viewname";
+        }
+        return $this->view($viewname, ['parse' => true]);
     }
 }
 
