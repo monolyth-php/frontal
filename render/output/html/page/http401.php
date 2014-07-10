aff9776401f1f00a42d18e09336c547d4a2173cb
Marijn Ophorst <marijn@sensimedia.nl>
2014-07-10 11:36:58 +0200

    401 page

---
+++ render/output/html/page/http401.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * The default 401 page.
+ *
+ * @package monolyth
+ * @subpackage render
+ * @author Marijn Ophorst <marijn@monomelodies.nl>
+ * @copyright MonoMelodies 2014
+ */
+
+namespace monolyth\render;
+
+?>
+<h1><?=$text('monolyth\401/title')?></h1>
+<p><?=$text('monolyth\401/explain')?></p>
+
