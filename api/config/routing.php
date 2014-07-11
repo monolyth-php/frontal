d991c77c9aef92ef35174c1ac5376ac69caebc3f
Marijn Ophorst <marijn@sensimedia.nl>
2014-07-11 14:59:47 +0200

    route for logging in

--- api/config/routing.php
+++ api/config/routing.php
@@ -6,6 +6,7 @@ return function($m) use($project) {
     $m->connect('/html/(%s:language)/(%a:viewname).html', 'monolyth\api\View');
     $m->connect('/monolyth/form/(%s:language)/(%a:name)/', 'monolyth\api\Form');
     $m->connect('/monolyth/session/', 'monolyth\api\Session');
+    $m->connect('/monolyth/account/login/', 'monolyth\api\Login');
     return $m;
 };
 
