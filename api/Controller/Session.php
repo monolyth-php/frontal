0ac9aa47c335246a9f67e80923766393fcaeebbc
Marijn Ophorst <marijn@sensimedia.nl>
2014-07-11 14:59:08 +0200

    session data needs some massaging before we want to expose it

--- api/Controller/Session.php
+++ api/Controller/Session.php
@@ -3,6 +3,7 @@
 namespace monolyth\api;
 use monolyth\Controller;
 use monolyth\HTTP404_Exception;
+use monolyth\User_Access;
 
 class Session_Controller extends Controller
 {
@@ -10,10 +11,24 @@ class Session_Controller extends Controller
 
     protected function get(array $args)
     {
-        return $this->view(
-            'monolyth\render\page/json',
-            ['data' => self::session()->all()]
-        );
+        $session = self::session()->all();
+        if (isset($session['User'])) {
+            foreach ([
+                'pass', 'salt', 'ipcreated', 'ipmodified', 'ipactive',
+            ] as $remove) {
+                unset($session['User'][$remove]);
+            }
+        }
+        if (isset($session['Groups'])) {
+            unset($session['Groups']);
+        }
+        return $this->view('monolyth\render\page/json', ['data' => $session]);
+    }
+
+    protected function post(array $args)
+    {
+        self::session()->stop();
+        return $this->view('monolyth\render\page/json', ['data' => 1]);
     }
 }
 
