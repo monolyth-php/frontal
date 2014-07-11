ed6a23451fc791ceed1b74e9c5965f253d5e964e
Marijn Ophorst <marijn@sensimedia.nl>
2014-07-11 15:00:28 +0200

    login via api

---
+++ api/Controller/Login.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace monolyth\api;
+use monolyth\account;
+use monolyth\HTTP301_Exception;
+use monolyth\HTTP404_Exception;
+use monolyth\render\FileNotFound_Exception;
+use monolyth\render\Ajax_Translate_Parser;
+
+class Login_Controller extends account\Login_Controller
+{
+    protected $template = false;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->translate = new Ajax_Translate_Parser;
+    }
+
+    protected function post(array $args)
+    {
+        try {
+            parent::post($args);
+        } catch (FileNotFound_Exception $e) {
+            // That's fine, we don't need the entire page anyway here.
+        } catch (HTTP301_Exception $e) {
+            // This we definitely don't want for ajaxy API controllers.
+        }
+        return $this->view(
+            'monolyth\render\page/json',
+            ['data' => self::user()->loggedIn()]
+        );
+    }
+}
+
