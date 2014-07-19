622ba7a26dda552a148d4ac2fd8227a4ca4dfc16
Marijn Ophorst <marijn@sensimedia.nl>
2014-07-19 11:29:10 +0200

    viewnames

--- account/output/html/page/login.php
+++ account/output/html/page/login.php
@@ -10,7 +10,7 @@ if (!(isset($form) && $form instanceof Login_Form)) {
     $form = new Login_Form;
 }
 echo $view(
-    ['monolyth\render\form\slice/table', 'monolyth\render\form\slice/form'],
+    ['monolyth\render\form\table', 'monolyth\render\form\form'],
     compact('form')
 );
 
