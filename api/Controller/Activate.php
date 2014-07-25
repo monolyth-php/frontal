70443f306baa34b883e1e540ba73c8d89087ec34
Marijn Ophorst <marijn@sensimedia.nl>
2014-07-25 08:51:58 +0200

    ajaxy activcate controller

---
+++ api/Controller/Activate.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace monolyth\api;
+use monolyth\account;
+use monolyth\HTTP301_Exception;
+use monolyth\render\FileNotFound_Exception;
+use ErrorException;
+
+class Active_Controller extends account\Do_Activate_Controller
+{
+    protected $template = false;
+
+    protected function post(array $args)
+    {
+        try {
+            parent::get(['id' => $_POST['id'], 'code' => $_POST['code']]);
+        } catch (FileNotFound_Exception $e) {
+            // That's fine, we don't need the entire page anyway here.
+        } catch (HTTP301_Exception $e) {
+            // This we definitely don't want for ajaxy API controllers.
+        } catch (ErrorException $e) {
+            // Prolly post was invalid.
+        }
+        return $this->view(
+            'monolyth\render\json',
+            ['data' => self::user()->active()]
+        );
+    }
+}
+
