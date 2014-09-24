b3465424844c6270663c91ba4d96337e530e5dbc
Marijn Ophorst <marijn@sensimedia.nl>
2014-09-24 09:41:17 +0200

    handle this more elegantly

--- api/Controller/Activate.php
+++ api/Controller/Activate.php
@@ -1,12 +1,14 @@
 <?php
 
 namespace monolyth\api;
-use monolyth\account;
+use monolyth\account\Do_Activate_Controller;
 use monolyth\HTTP301_Exception;
+use monolyth\Ajax_Login_Required;
 use monolyth\render\FileNotFound_Exception;
 use ErrorException;
 
-class Activate_Controller extends account\Do_Activate_Controller
+class Activate_Controller extends Do_Activate_Controller
+implements Ajax_Login_Required
 {
     protected $template = false;
 
@@ -23,7 +25,7 @@ class Activate_Controller extends account\Do_Activate_Controller
         }
         return $this->view(
             'monolyth\render\json',
-            ['data' => self::user()->active()]
+            ['data' => self::user()->active() ? 1 : 0]
         );
     }
 }
