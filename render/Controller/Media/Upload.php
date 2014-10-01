2bbf82183705e1ca81a35d5933f62d766fbb6efa
Marijn Ophorst <marijn@sensimedia.nl>
2014-10-01 21:23:18 +0200

    handle uploading, sort-of i guess

--- render/Controller/Media/Upload.php
+++ render/Controller/Media/Upload.php
@@ -5,6 +5,7 @@ use monolyth\Controller;
 use monolyth\Ajax_Login_Required;
 use monolyth\HTTP400_Exception;
 use monolyth\HTTP500_Exception;
+use monolyth\Config;
 
 class Upload_Media_Controller extends Controller
 {
@@ -12,9 +13,20 @@ class Upload_Media_Controller extends Controller
 
     protected function post(array $args)
     {
+        $config = Config::get('monolyth');
+        $name = substr(
+            $_FILES['file']['tmp_name'],
+            strrpos($_FILES['file']['tmp_name'], '/') + 1
+        );
+        // Todo: cleanup old files in this directory, anything older than
+        // 24 hours can be considered obsolete by now...
+        move_uploaded_file(
+            $_FILES['file']['tmp_name'],
+            "{$config->uploadPath}/$name"
+        );
         return $this->view(
             'monolyth\render\json',
-            ['data' => ['file' => $_FILES['file']['tmp_name']]]
+            ['data' => ['file' => "{$config->uploadPath}/$name"]]
         );
     }
 }
