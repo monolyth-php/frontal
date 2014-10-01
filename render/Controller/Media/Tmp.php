aca26725a5c477ae9651de1d9559453653a0410d
Marijn Ophorst <marijn@sensimedia.nl>
2014-10-01 21:01:31 +0200

    cleanup and use better exceptions

--- render/Controller/Media/Tmp.php
+++ render/Controller/Media/Tmp.php
@@ -3,20 +3,21 @@
 namespace monolyth\render;
 use monolyth\Controller;
 use monolyth\Login_Required;
-use monolyth\HTTP404_Exception;
+use monolyth\HTTP400_Exception;
+use monolyth\HTTP500_Exception;
 
 class Tmp_Media_Controller extends Controller implements Login_Required
 {
     protected function get(array $args)
     {
         if (!isset($_GET['f'])) {
-            throw new HTTP404_Exception();
+            throw new HTTP400_Exception;
         }
         $_GET['f'] = urldecode($_GET['f']);
         try {
             $i = getimagesize($_GET['f']);
         } catch (ErrorException $e) {
-            throw new HTTP404_Exception();
+            throw new HTTP500_Exception;
         }
         $this->template = null;
         return $this->view(
