27f544f2f679eb894e31541fa8a66c343629ef8a
Marijn Ophorst <marijn@sensimedia.nl>
2015-01-12 10:16:11 +0100

    improve autoloader, and add some other bootstrap stuff

--- bootstrap.php
+++ bootstrap.php
@@ -2,16 +2,22 @@
 
 namespace monolyth;
 
+/**
+ * Register the Monolyth autoloader.
+ *
+ * All lowercase namespaces are considered a "submodule", which allows
+ * us to place submodules in their own repository but still fall under the
+ * global namespace of their "parent" module. For example:
+ * In the namespace monolyth we have authentication helpers in
+ * monolyth\auth. To allow separate cloning and substitution by the same
+ * name, we store this in monolyth-auth instead.
+ * If a "submodule" could not be found, try the "normal" version instead
+ * (which just treats all lowercase namespaces as directories verbatim).
+ */
 spl_autoload_register(function($class) {
     $namespaces = explode('\\', $class);
     $class = array_pop($namespaces);
 
-    // All lowercase namespaces are considered a "submodule", which allows
-    // us to place submodules in their own repository but still fall under the
-    // global namespace of their "parent" module. For example:
-    // In the namespace monolyth we have authentication helpers in
-    // monolyth\auth. To allow separate cloning and substitution by the same
-    // name, we store this in monolyth-auth instead.
     $modules = [];
     while (true) {
         if (!$namespaces) {
@@ -23,19 +29,42 @@ spl_autoload_register(function($class) {
             break;
         }
     }
-    $file = sprintf(
-        '%s%s%s%s%s.php',
-        implode('-', $modules),
-        $modules ? DIRECTORY_SEPARATOR : '',
-        implode(DIRECTORY_SEPARATOR, $namespaces),
-        $namespaces ? DIRECTORY_SEPARATOR : '',
-        $class
-    );
-    // Use fopen so it supports include_path:
-    $fp = @fopen($file, 'r', true);
-    if ($fp) {
-        fclose($fp);
-        include $file;
+    foreach (['-', DIRECTORY_SEPARATOR] as $type) {
+        $file = sprintf(
+            '%s%s%s.php',
+            $modules ? implode($type, $modules).DIRECTORY_SEPARATOR : '',
+            $namespaces ?
+                implode(DIRECTORY_SEPARATOR, $namespaces).DIRECTORY_SEPARATOR :
+                '',
+                $class
+        );
+        // Use fopen so it supports include_path:
+        $fp = @fopen($file, 'r', true);
+        if ($fp) {
+            fclose($fp);
+            include $file;
+            return;
+        }
     }
 });
+
+/** Turn on all errors so we can catch exceptions. */
+error_reporting(E_ALL & ~E_STRICT);
+/** Define the generic error handler. */
+set_error_handler(
+    function($errno, $errstr, $errfile, $errline, $errcontext) {
+        throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
+    },
+    error_reporting()
+);
+
+/** Required to make PHP handle UTF8 in a sane way. */
+if (function_exists('mb_internal_encoding')) {
+    mb_internal_encoding('UTF-8');
+}
+if (function_exists('mb_detect_order')) {
+    mb_detect_order([
+        'CP1251', 'CP1252', 'ISO-8859-1', 'UTF-8',
+    ]);
+}
 
