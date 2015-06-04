867a44eb7e61fe6d9d8def878c497aadc6db22a2
Marijn Ophorst <marijn@sensimedia.nl>
2015-06-04 16:37:17 +0200

    cleanup

--- info/skeleton/example.com/www/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-call_user_func(function($dir) {
-    require_once 'monolyth/Monolyth.php';
-    $project = Project::instance();
-    $project['public'] = $dir;
-    Monolyth::run($project);
-}, realpath(__DIR__));
-
