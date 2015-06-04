52d99852da09b510b00dae0a7fa0fcc654b5e6d9
Marijn Ophorst <marijn@sensimedia.nl>
2015-06-04 16:33:42 +0200

    cleanup

--- config/admin.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-monad\Admin::register('monolyth\Language');
-monad\Admin::register('monolyth\Text');
-monad\Admin::register('monolyth\Mail');
-monad\Admin::register('monolyth\Mail_Template');
-
