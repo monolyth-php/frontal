cd8dc5c1d6bd74617ac094bc0356799763117e5d
Marijn Ophorst <marijn@sensimedia.nl>
2015-06-22 17:06:07 +0200

    throw logical exception

--- httpdocs/index.php
+++ httpdocs/index.php
@@ -13,10 +13,12 @@ require_once '/path/to/src/dependencies.php';
 require_once '/path/to/src/routing.php';
 
 try {
-    $state = $router->resolve(
+    if (!($state = $router->resolve(
         $_SERVER['REQUEST_URI'],
         $_SERVER['REQUEST_METHOD']
-    );
+    ))) {
+        throw new Exception;
+    }
     switch ($state->group()) {
         default:
             echo $state->run();
