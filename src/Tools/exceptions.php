0fde7f32e4cd9d87a58c78f0df296461ec19f1ce
Marijn Ophorst <marijn@sensimedia.nl>
2015-08-24 08:00:39 +0200

    explain

--- src/Tools/exceptions.php
+++ src/Tools/exceptions.php
@@ -10,11 +10,22 @@
  */
 namespace Monolyth\Tools;
 
+/**
+ * Sometimes it can be handy to convert all "traditional" PHP errors to
+ * exceptions. Simply include this file and from then on, 't shall be done.
+ *
+ * Caution: not every external library will expect this behaviour, so be
+ * prepared to break stuff here and there when using this.
+ */
 use ErrorException;
 
-/** Turn on all errors so we can catch exceptions. */
+/**
+ * Turn on all errors so we can catch exceptions.
+ */
 error_reporting(E_ALL & ~E_STRICT);
-/** Define the generic error handler. */
+/**
+ * Define the generic error handler.
+ */
 set_error_handler(
     function($errno, $errstr, $errfile, $errline, $errcontext) {
         throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
