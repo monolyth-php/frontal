25d75a05a5fd2d5c669f781699b0ae8343df9b80
Marijn Ophorst <marijn@sensimedia.nl>
2015-09-29 09:30:10 +0200

    move these here

--- httpdocs/index.php
+++ httpdocs/index.php
@@ -8,6 +8,34 @@
 // Require and setup the Composer autoloader:
 $autoloader = require_once '../vendor/autoload.php';
 
+/**
+ * Required to make PHP handle UTF8 in a sane way. To use, simply include
+ * somewhere early in your front controller (typically index.php).
+ */
+if (function_exists('mb_internal_encoding')) {
+    mb_internal_encoding('UTF-8');
+}
+if (function_exists('mb_detect_order')) {
+    mb_detect_order([
+        'CP1251', 'CP1252', 'ISO-8859-1', 'UTF-8',
+    ]);
+}
+
+/**
+ * Correct REMOTE_ADDR if we're behind a proxy.
+ * This code is by no means extensive; there's prolly 1.000 other
+ * cases you'll want to handle.
+ *
+ * To use, simply include this somewhere early in your front controller
+ * (typically index.php).
+ */
+if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+    $_SERVER['REMOTE_ADDR'] = trim(array_shift(explode(
+       ',',
+        $_SERVER['HTTP_X_FORWARDED_FOR']
+    )));
+}
+
 // Dependencies and routing:
 require_once '../src/dependencies.php';
 require_once '../src/routing.php';
