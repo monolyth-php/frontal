f1aa3e1faca5a0df1f904fde98d5f277958fdfa2
Marijn Ophorst <marijn@sensimedia.nl>
2015-09-29 09:31:08 +0200

    better explanation, and move injection to dependencies example

--- src/View.php
+++ src/View.php
@@ -2,10 +2,10 @@
 
 /**
  * This is an example base view you can extend for all your pages. It uses Twig,
- * so you need to set that up correctly in your dependencies.
+ * so you need to set that up correctly in your dependencies. See
+ * `./src/dependencies.php` for an example.
  */
 use Disclosure\Injector;
-use Disclosure\Container;
 use Improse;
 
 abstract class View extends Improse\View
@@ -17,6 +17,10 @@ abstract class View extends Improse\View
     
     public function __construct()
     {
+        /**
+         * This uses the Disclosure Injector to inject $twig into a member.
+         * See the Disclosure documentation for more information.
+         */
         $this->inject(function ($twig) {});
     }
     
@@ -25,37 +29,4 @@ abstract class View extends Improse\View
         return $this->twig->render($this->template, $__viewdata);
     }
 }
-
-/**
- * Example injection of Twig into your views (we don't want to make any
- * assumptions about your path structure etc.:
- */
-/*
-View::inject(function (&$twig) {
-    $loader = new Twig_Loader_Filesystem('/path/to/code');
-    $twig = new Twig_Environment($loader, [
-        'cache' => '/path/to/cache',
-        'debug' => true|false,
-        'auto_reload' => true|false,
-    ]);
-    
-    // Example url function, assuming `$router` is available:
-    $url = function ($name, array $args = []) use ($router) {
-        if (!isset($_SERVER['SERVER_NAME'])) {
-            $_SERVER['SERVER_NAME'] = 'http://localhost';
-        }
-        return $router->get($name)->url()->short(
-            "http://{$_SERVER['SERVER_NAME']}{$_SERVER['REQUEST_URI']}",
-            $args
-        );
-    };
-    $twig->addFunction(new Twig_SimpleFunction('url', $url));
-    
-    // Example integration of Metaculous:
-    $twig->addExtension(new Metaculous\TwigExtension(
-        $arrayOfIgnoreWords,
-        $hashOfRiggedWords
-    ));
-});
-*/
 
