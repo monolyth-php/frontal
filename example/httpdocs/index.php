474b0cee0efa6785aa9ccb0037379be6f06be941
Marijn Ophorst <marijn@sensimedia.nl>
2015-11-28 12:53:20 +0100

    improve example index.php

--- example/httpdocs/index.php
+++ example/httpdocs/index.php
@@ -8,39 +8,23 @@
 use Disclosure\Container;
 use Cesession\Session;
 use Cesession\Handler;
+use Monolyth\Utilities;
+use Monolyth\HttpController;
+use League\Pipeline\Pipeline;
 
-// Require and setup the Composer autoloader:
+/** Require and setup the Composer autoloader. */
 $autoloader = require_once '../vendor/autoload.php';
 
-/**
- * Required to make PHP handle UTF8 in a sane way. To use, simply include
- * somewhere early in your front controller (typically index.php).
- */
-if (function_exists('mb_internal_encoding')) {
-    mb_internal_encoding('UTF-8');
-}
-if (function_exists('mb_detect_order')) {
-    mb_detect_order([
-        'CP1251', 'CP1252', 'ISO-8859-1', 'UTF-8',
-    ]);
-}
-
-/**
- * Correct REMOTE_ADDR if we're behind a proxy.
- * This code is by no means extensive; there's prolly 1.000 other
- * cases you'll want to handle.
- *
- * To use, simply include this somewhere early in your front controller
- * (typically index.php).
- */
-if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-    $_SERVER['REMOTE_ADDR'] = trim(array_shift(explode(
-       ',',
-        $_SERVER['HTTP_X_FORWARDED_FOR']
-    )));
-}
+/** @see Monolyth\Utilities::utf8 */
+Utilities::utf8();
+/** @see Monolyth\Utilities::proxy */
+Utilities::proxy();
 
 require_once '../src/dependencies.php';
+
+$pipeline = new Pipeline;
+$controller = new HttpController;
+$controller->run();
 
 /**
  * We use Cesession sessions by default; remove or change this if your
