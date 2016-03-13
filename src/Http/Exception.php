b908b70864f171572d34fe0ab920c3bfecd5c79f
Marijn Ophorst <marijn@sensimedia.nl>
2016-03-13 10:42:34 +0100

    persist the message, if given

--- src/Http/Exception.php
+++ src/Http/Exception.php
@@ -6,7 +6,11 @@ class Exception extends \Exception
 {
     public function __construct($code = 0, \Exception $previous = null)
     {
-        parent::__construct('', $code, $previous);
+        $message = '';
+        if (isset($previous)) {
+            $message = $previous->getMessage();
+        }
+        parent::__construct($message, $code, $previous);
     }
 }
 
