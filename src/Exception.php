4f126c4ec2514edb649556d9ffe2d7afb4a9f225
Marijn Ophorst <marijn@sensimedia.nl>
2025-02-26 16:25:05 +0100

    explicitly make this nullable

--- src/Exception.php
+++ src/Exception.php
@@ -4,7 +4,7 @@ namespace Monolyth\Frontal;
 
 class Exception extends \Exception
 {
-    public function __construct($code = 0, \Exception $previous = null)
+    public function __construct($code = 0, ?\Exception $previous = null)
     {
         $message = '';
         if (isset($previous)) {
